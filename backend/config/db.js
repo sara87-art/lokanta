db7a43a86d51df427e7becf3acd905fb0d593208
Sara Mustafa <sarakir87@gmail.com>
2026-08-09 02:29:19 +0300

    Configure MySQL SSL connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,6 +6,9 @@ const db = mysql.createConnection({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: true
+  }
 });
 
 db.connect((err) => {
